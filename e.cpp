d94dc0fc5a0cf1e2c99023d2cf38ece4d2711e7e
Diego Pisani <diego.luidiblu@gmail.com>
2020-02-01 19:53:15 -0300

    e

---
+++ e.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define e using
+#define ee namespace
+#define eee std
+#define eeee ;
+#define eeeee int
+#define eeeeee main
+#define eeeeeee (
+#define eeeeeeee )
+#define eeeeeeeee while
+#define eeeeeeeeee true
+#define eeeeeeeeeee {
+#define eeeeeeeeeeee }
+#define eeeeeeeeeeeee cout
+#define eeeeeeeeeeeeee cerr
+#define eeeeeeeeeeeeeee « 
+#define eeeeeeeeeeeeeeee 'e'
+#define eeeeeeeeeeeeeeeee return 
+
+e ee eee eeee
+eeeeeeeee eeeeeee eeeeeeeeee eeeeeeee
+eeeeeeeeeee
+eeeeeeeeeeee
+eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeee eeee
